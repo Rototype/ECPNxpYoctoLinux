7c26e866b205798b4dcd1047cc6b9fc7d3ba65f8
Claudio Santacesaria <claudio.santacesaria@rototype.com>
2021-10-27 12:24:55 +0200

    Fix program i210 script

--- recipes-core/rototypescript/files/prI210.sh
+++ recipes-core/rototypescript/files/prI210.sh
@@ -4,7 +4,7 @@ file_i210="/home/root/update/mac.addr"
 
 if [ -f "$file_i210" ] 
 then
-	EepromAccessTool -NIC=1 -F=/home/root/firmware/I210_Invm_Copper_NoAPM_v0.6.HEX -MAC=cat $file_i210
+	EepromAccessTool -NIC=1 -F=/home/root/firmware/I210_Invm_Copper_NoAPM_v0.6.HEX -MAC=$(cat $file_i210)
 fi
 
 
