67a5924e83f09e052a9cc7735b59175d5c01cd6f
Claudio Santacesaria <claudio.santacesaria@rototype.com>
2021-10-27 12:36:54 +0200

    Launching communicator (with different strategies)

--- recipes-core/rototypescript/files/startEcpApp.sh
+++ recipes-core/rototypescript/files/startEcpApp.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-sleep 10
 echo "Launching Comunicator"
 cd /home/root/
-/home/root/app/Comunicator
 
+# nohup without logs
+nohup /home/root/app/Comunicator > /dev/null 2>&1 &
+
+# nohup.out with logs for debugging
+#nohup /home/root/app/Comunicator &
+
+# Alternative solution
+#sleep 10
+#/usr/bin/openvt -c5 -f -- /home/root/app/Comunicator
+
+# Simple launch without log
+#/home/root/app/Comunicator &
+
+# Simple launch with log
+#/home/root/app/Comunicator >> comunicatorlog.txt 2>> comunicatorlog.txt &
 
 
